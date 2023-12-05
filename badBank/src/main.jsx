d342a8eecbef54ce4ae4898ccf7d40d7718fab2e
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2023-12-04 20:34:23 -0500

    added bootstrap

--- badBank/src/main.jsx
+++ badBank/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// Import our custom CSS
+import './main.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
