e923c2a6e61c18d96e0118630eb3c4773582fd1e
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-14 12:27:39 -0500

    fixed navbar dropdown

--- BadBankStandAlone/navbar.js
+++ BadBankStandAlone/navbar.js
@@ -1,9 +1,9 @@
 function NavBar(){
   return(
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <a className="navbar-brand" href="#">BadBank</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
