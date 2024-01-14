21b4b79b0d05ec88ba829dc535217ffa47727c58
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-14 11:29:27 -0500

    added show to toggle user and balance when noone is logged in

--- BadBankStandAlone/balance.js
+++ BadBankStandAlone/balance.js
@@ -2,6 +2,7 @@ function Balance(){
   const { useState, useEffect }             = React;
   const [balance, setBalance]               = useState()
   const [user, setUser]                     = useState()
+  const [show, setShow]                     = useState()
   const ctx = React.useContext(UserContext);
 
   useEffect(() => {
@@ -11,19 +12,26 @@ function Balance(){
           let tempUser = [ctx[1].users[i]];
           setUser(tempUser[0].name);
           setBalance(tempUser[0].balance);
+          loggedIn()
         }
       })
     }
     getLoggedInUser();
   }, []);
 
+  const loggedIn = () => {
+    if (ctx[0].loggedIn == 'none') {
+      setShow(false)
+    } else setShow(true)
+  }
+
   return (
     <Card
-          bgcolor='dark'
-          txtcolor="white"
-          header={`User : ${user}`}
-          title='balance'
-          body={balance}
-        />
+      bgcolor='dark'
+      txtcolor="white"
+      header={ show ? `User : ${user}` : `Please Login`}
+      title='Balance'
+      body={ show ? `$${balance}` : `No User Logged In`}
+    />
   )
 }
