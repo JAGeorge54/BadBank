f7b05f88ffaf1dec2761bbdc85904356d75ec8a8
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-14 10:04:20 -0500

    fixed cards and fixed colors

--- BadBankStandAlone/alldata.js
+++ BadBankStandAlone/alldata.js
@@ -1,30 +1,20 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
-  let cardInfo = ctx[1].users.map((user, i) => {
-    return <Card
-    key={i}
-    txtcolor="black"
-    header={user.name}
-    title={user.email}
-    body={<ul>
-      <li>Password: {user.password}</li>
-      <li>Balnce: ${user.balance}</li>
-    </ul>}
-    />
-  })
-
   function MakeCard(users) {
     let context = ctx[1].users;
     const card = context.map((user, i) => {
       return (
-        <div key={i} className='card'>
-          <h1 className='card-header'>{user.name}</h1>
-          <ul>
-            <li>User:{user.email}</li>
-            <li>User:{user.password}</li>
-            <li>User:{user.balance}</li>
-          </ul>
-        </div>
+        <Card
+          key={i}
+          bgcolor='dark'
+          txtcolor="white"
+          header={`User : ${user.name}`}
+          title={`Email : ${user.email}`}
+          body={<ul>
+                  <li>Password : {user.password}</li>
+                  <li>Balnce : ${user.balance}</li>
+                </ul>}
+        />
       )
     })
     return card
@@ -33,7 +23,6 @@ function AllData(){
     <>
       <h5>All Data in Store</h5>
       {/* {JSON.stringify(ctx)}<br/> */}
-      {cardInfo}
       <MakeCard/>
     </>
   );
