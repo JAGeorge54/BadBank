c64a79048196238284746cf98a93ffbead2b467f
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-14 11:11:21 -0500

    added card for balance page

--- BadBankStandAlone/balance.js
+++ BadBankStandAlone/balance.js
@@ -1,5 +1,29 @@
 function Balance(){
+  const { useState, useEffect }             = React;
+  const [balance, setBalance]               = useState()
+  const [user, setUser]                     = useState()
+  const ctx = React.useContext(UserContext);
+
+  useEffect(() => {
+    const getLoggedInUser = () => {
+      ctx[1].users.map((user, i) => {
+        if(user.name == ctx[0].loggedIn){
+          let tempUser = [ctx[1].users[i]];
+          setUser(tempUser[0].name);
+          setBalance(tempUser[0].balance);
+        }
+      })
+    }
+    getLoggedInUser();
+  }, []);
+
   return (
-    <h1>Balance</h1>
+    <Card
+          bgcolor='dark'
+          txtcolor="white"
+          header={`User : ${user}`}
+          title='balance'
+          body={balance}
+        />
   )
 }
