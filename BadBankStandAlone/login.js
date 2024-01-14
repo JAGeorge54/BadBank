764cee4c2e789fd1ed7a54a97b279bfb5291c151
JAGeorge54 <137643997+JAGeorge54@users.noreply.github.com>
2024-01-14 10:21:02 -0500

    spacing, simi colons, and removed forms

--- BadBankStandAlone/login.js
+++ BadBankStandAlone/login.js
@@ -1,11 +1,10 @@
 function Login(){
-  const ctx = React.useContext(UserContext);
-  const [name, setName] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [status, setStatus] = React.useState(false);
+  const [name, setName]                 = React.useState('');
+  const [password, setPassword]         = React.useState('');
+  const [status, setStatus]             = React.useState(false);
   const [loggedInUser, setLoggedInUser] = React.useState(false);
-  const [display, setDisplay] = React.useState(true);
-  const users = ctx[1].users
+  const ctx = React.useContext(UserContext);
+  const users = ctx[1].users;
 
 
 const validateUser = (user) => {
@@ -29,8 +28,8 @@ const logInUser = () => {
   if(!validateForm(name, 'name')) return
   if(!validateForm(password, 'password')) return
   let validUser = users.filter(validateUser);
-  ctx[0].loggedIn = validUser[0].name
-  setLoggedInUser(true)
+  ctx[0].loggedIn = validUser[0].name;
+  setLoggedInUser(true);
 
 }
 
@@ -41,16 +40,16 @@ const logInUser = () => {
       status={status}
       body={!loggedInUser ? (
         <>
-          <form >
+          
             <label>Name</label>
             <br />
-            <input placeholder='Enter Name' onChange={e => setName(e.target.value)}></input>
+            <input placeholder='Enter name' onChange={e => setName(e.target.value)}></input>
             <label>Password</label>
-            <input placeholder='Enter Password' onChange={e => setPassword(e.target.value)}></input>
+            <input placeholder='Enter password' onChange={e => setPassword(e.target.value)}></input>
             <br />
             <br />
-            <button onClick={logInUser} type="submit" className="btn btn-light">Create Account</button>
-          </form>
+            <button onClick={logInUser} type="submit" className="btn btn-light">Login</button>
+          
         </>
       ) : (
         <>
